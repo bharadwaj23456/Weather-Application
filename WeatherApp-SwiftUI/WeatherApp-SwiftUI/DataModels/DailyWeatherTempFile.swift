aee6b23b4759708f08b8c1a3fc19c6d220ec7ba8
bharadwaj23456 <79959667+bharadwaj23456@users.noreply.github.com>
2024-08-27 10:48:49 +0530

    Add files via upload

---
+++ WeatherApp-SwiftUI/WeatherApp-SwiftUI/DataModels/DailyWeatherTempFile.swift
@@ -0,0 +1,13 @@
+//
+//  DailyWeatherTemp.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Reddy Bharadwaj Chowdary on 07/01/24.
+//
+import Foundation
+
+struct DailyWeatherTemp: Codable {
+    
+    var min: Double
+    var max: Double
+}
---
+++ WeatherApp-SwiftUI/WeatherApp-SwiftUI/NetworkManager/LocationManager.swift
@@ -0,0 +1,73 @@
+//
+//  LocationManager.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Reddy Bharadwaj Chowdary on 07/01/24.
+//
+
+import CoreLocation
+import Combine
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    static let shared = LocationManager()
+
+        @Published var currentLocation: CLLocationCoordinate2D?
+        @Published var isAuthorized: Bool = false
+
+        private var locationManager: CLLocationManager
+        private var locationUpdateCompletion: ((CLLocationCoordinate2D?) -> Void)?
+
+    private override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        setupLocationManager()
+    }
+    
+
+    func requestAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    private func setupLocationManager() {
+        locationManager.delegate = self
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            requestAuthorization()
+        } else {
+            print("Location services are not enabled.")
+        }
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        isAuthorized = manager.authorizationStatus == .authorizedWhenInUse
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        case .denied, .restricted:
+            print("Location access denied or restricted.")
+        case .notDetermined:
+            print("Location authorization status not determined yet.")
+        @unknown default:
+            fatalError("Unhandled case in locationManagerDidChangeAuthorization.")
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last?.coordinate else { return }
+        currentLocation = location
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager error: \(error.localizedDescription)")
+    }
+    func getCurrentLocation(completion: @escaping (CLLocationCoordinate2D?) -> Void) {
+            if let location = locationManager.location?.coordinate {
+                completion(location)
+            } else {
+                locationManager.requestLocation()
+            
+                self.locationUpdateCompletion = completion
+            }
+        }
+}
